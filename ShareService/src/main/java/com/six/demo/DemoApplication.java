2af4ab3a265cfd9caf4b7960fa7ce404f48cc20a
lcelik <lidijacelik87@gmail.com>
2022-12-06 12:27:47 +0000

    pushing Share Service

--- ShareService/src/main/java/com/six/demo/DemoApplication.java
+++ ShareService/src/main/java/com/six/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.six.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.six")
+@EntityScan(basePackages = "com.six.entity")
+@EnableJpaRepositories(basePackages = "com.six.persistence")
 public class DemoApplication {
 
 	public static void main(String[] args) {
